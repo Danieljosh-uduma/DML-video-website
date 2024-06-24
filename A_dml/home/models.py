6c483cc524d87e2bc5fcaa95cdbe361844e780fd
Daniel Josh Uduma <116913814+Danieljosh-uduma@users.noreply.github.com>
2024-06-24 23:40:13 +0100

    Update models.py
    update

--- A_dml/home/models.py
+++ A_dml/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class FAQs(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=600)
+    created = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.title)
